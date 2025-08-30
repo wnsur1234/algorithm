61f17e3c06a23d1055c70097d3b39f471d1e1061
Jun <wnsur1234@gmail.com>
2025-08-31 00:20:54 +0900

    :construction: fire: Fail to Solution about LeastMuliple

--- src/com/proCode/level2/LeastMultiple.java
+++ src/com/proCode/level2/LeastMultiple.java
@@ -1,7 +1,9 @@
 package com.proCode.level2;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class LeastMultiple {
     public static void main(String[] args) {
@@ -30,17 +32,27 @@ public class LeastMultiple {
     }
 
     private static int solution(int[] arr) {
-        /*
-        생각 1 : 각 수를 반복하면서 n을 무한으로 반복 모든 값이 같아 진다면 멈춤
-        ex) 2 -> 1부터 n까지 의 곱 값
-            6,8,14 -> 동일           -> 같아지는 값 stop
-//        * */
-//        while(true){
-//            for(int i = 0; i < arr.length; i++){
-//
-//
-//            }
-//        }
-        return 1;
+        Set<Integer> set = new HashSet<>();
+        int i = 1;
+        while(true) {
+            for (int j : arr) {
+                System.out.println(j);
+                // 각요소에 같은 값을 곱하여 set에 추가함
+                set.add(j * i);
+            }
+            // 근데 set의 특성상 중복을 허용하지 않기 때문에
+            // 같은 값을 추가할 경우 그 값만 남아 있을 것
+            // 따라서 최소공배수인 각 요소에 곱한 값이 똑같다면 size는 1일거라 생각
+            if (set.size() == 1) {
+                break;
+            } else {
+                // 그랬는데 1이 아니라면 set자체를 아예 비우고 다음 곱해지는 요소를 +1하여 다시 set에 추가하기 위함
+                set.clear();
+                i++;
+            }
+            System.out.println(i);
+        }
+        // j*i의 값 즉 현재 최소공배수로 add 된 값 예상
+        return set.hashCode();
     }
 }
